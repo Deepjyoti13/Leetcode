e45a9a74483a301b5af96242ec4a108a171a7087
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-05-31 02:24:30 +0530

    Time: 51 ms (74.90%), Space: 18.9 MB (68.50%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool isValid(vector<int>& piles, long long mid, int h) {
+        long long k=0, sum=0;
+        for(int i: piles) {
+            k+=i/mid;
+            if(i%mid) k++;
+            
+            if(k>h) return false;   
+        }        
+        return true;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int res=0;
+        long long low = 1, high = 1000000000;
+        while(low<=high) {
+            long long mid = low+(high-low)/2;
+            if(isValid(piles, mid, h)) {
+                res = (int) mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3 4 5 6 7 8 9 10 11
+
