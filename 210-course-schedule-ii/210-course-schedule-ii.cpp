42818c9f755bfa7cd4f5bc076b173aba1b46f23c
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-05 01:05:33 +0530

    Time: 23 ms (81.21%), Space: 15.1 MB (17.70%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    
+    vector<vector<int>> buildGraph(int N, vector<vector<int>>& values) {
+        vector<vector<int>> adjList(N);
+        for(vector<int> i: values) {
+            adjList[i[1]].push_back(i[0]);
+        }
+        return adjList;
+    }
+    
+    bool DFS(vector<vector<int>>& grid, int sv, int N, vector<int>& visited, stack<int>& order) {
+        visited[sv]=1;
+        for(int i: grid[sv]) {
+            if(visited[i]==1) return true;
+            if(!visited[i])
+                if(DFS(grid, i, N, visited, order)) return true;
+        }
+        visited[sv]=2;
+        order.push(sv);
+        return false;
+    }
+public:
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {                         vector<vector<int>> grid = buildGraph(numCourses, prerequisites);    
+        vector<int> visited(numCourses, 0);
+
+        stack<int> order;
+        for(int i=0; i<numCourses; i++) {
+            if(!visited[i]) {
+                if(DFS(grid, i, numCourses, visited, order))
+                    return {};
+            }
+        }
+        vector<int> res;
+        for(int i=0; i<numCourses; i++)
+        {
+            res.push_back(order.top());
+            order.pop();
+        }
+        return res;
+    }
+};
