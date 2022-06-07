079f5d85ce92f39fb6c6d178246bd6bb1179cd85
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-07 13:19:01 +0530

    Time: 7 ms (86.55%), Space: 11.8 MB (48.32%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -5,11 +5,13 @@ public:
         if(N<2) return 0;
         vector<vector<int>> dp(x+1, vector<int>(N, 0));
         for(int i=1; i<=x; i++) {
+            int mx = dp[i-1][0]-prices[0];
             for(int j=1; j<N; j++) {
-                dp[i][j]=dp[i][j-1];
-                for(int k=0; k<j; k++) {
-                    dp[i][j]=max(dp[i][j], dp[i-1][k]+prices[j]-prices[k]);
-                }
+                mx=max(mx, dp[i-1][j-1]-prices[j-1]);
+                dp[i][j]=max(dp[i][j-1], mx+prices[j]);
+                // for(int k=0; k<j; k++) {
+                //     dp[i][j]=max(dp[i][j], dp[i-1][k]+prices[j]-prices[k]);
+                // }
                 res=max(res, dp[i][j]);
             }
         }
