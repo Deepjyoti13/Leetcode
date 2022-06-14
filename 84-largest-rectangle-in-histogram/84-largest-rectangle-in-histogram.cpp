15ed7d76f86ffa37f429eac37c3e69cc730e3b25
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-15 02:32:50 +0530

    Time: 235 ms (59.36%), Space: 82.2 MB (30.24%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,22 +3,21 @@ public:
     int largestRectangleArea(vector<int>& nums) {
         int N = nums.size(), res=0;
         if(N==1) return nums[0];
-        stack<pair<int, int>> stk;
+        stack<pair<int, int>> stkL, stkR;
         vector<int> NSL(N, 0), NSR(N, 0);
-        // NSR
-        for(int i=N-1; i>=0; i--) {
-            while(!stk.empty() && stk.top().first>=nums[i])
-                stk.pop();
-            stk.empty()?NSR[i]=N:NSR[i]=stk.top().second;
-            stk.push({nums[i], i});
-        }
-        stk = {};
-        // NSL
+        
+        // NSL && NSR
         for(int i=0; i<N; i++) {
-            while(!stk.empty() && stk.top().first>=nums[i])
-                stk.pop();
-            stk.empty()?NSL[i]=-1:NSL[i]=stk.top().second;
-            stk.push({nums[i], i});
+            while(!stkL.empty() && stkL.top().first>=nums[i])
+                stkL.pop();
+            stkL.empty()?NSL[i]=-1:NSL[i]=stkL.top().second;
+            stkL.push({nums[i], i});
+            int j = N-i-1;
+            while(!stkR.empty() && stkR.top().first>=nums[j])
+                stkR.pop();
+            stkR.empty()?NSR[j]=N:NSR[j]=stkR.top().second;
+            stkR.push({nums[j], j});
+            
         }
         // calculation
         for(int i=0; i<N; i++) {
