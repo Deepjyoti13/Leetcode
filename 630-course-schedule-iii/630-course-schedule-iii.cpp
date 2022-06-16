63bf2e8f3919d209e54ab1df99f654dd3e0d4261
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-17 03:16:40 +0530

    Time: 416 ms (30.01%), Space: 63.5 MB (26.95%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        sort(courses.begin(), courses.end(), [](auto &a, auto &b) {return a[1] < b[1];});           priority_queue<int> maxHeap;
+        sort(courses.begin(), courses.end(), [](auto &a, auto &b) {return a[1]<b[1];});
+        priority_queue<int> maxHeap;
         int total_duration=0;
         for(vector<int> course: courses) {
             int duration = course[0], lastDay = course[1];
