4075e76f33d0625557094db6f9c57146653e2ffa
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-21 02:57:17 +0530

    Time: 524 ms (63.51%), Space: 8.3 MB (86.81%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,25 +1,21 @@
 class Solution {
+    int dp[301][301];
+    int helper(vector<int>& nums, int i, int j) {
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int mx = 0;
+        for(int k = i; k<=j; k++) {
+            mx = max(mx, helper(nums, i, k-1)+helper(nums, k+1, j)+nums[i-1]*nums[k]*nums[j+1]);
+        }
+        return dp[i][j] = mx;
+    }
 public:
     int maxCoins(vector<int>& nums) {
         int N = nums.size();
-        if(N==1) return nums[0];
-        if(N==2) return nums[0]*nums[1]+max(nums[0], nums[1]);
-        vector<vector<int>> dp(N, vector<int>(N, 0));
-
-        for(int i=N-1; i>=0; i--) {
-            for(int j=i; j<N; j++) {
-                for(int k=i; k<=j; k++) {
-                    int a=0, b=0, c=nums[k];
-                    if(k!=0) a = dp[i][k-1];
-                    if(i!=0) c=c*nums[i-1];
-                    if(k!=N-1) b = dp[k+1][j];
-                    if(j!=N-1) c=c*nums[j+1];
-                    dp[i][j] = max(dp[i][j], a + c + b);
-                }
-            }
-        }
-
-        return dp[0][N-1];
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        memset(dp, -1, sizeof(dp));
+        return helper(nums, 1, N);
     }
 };
 
