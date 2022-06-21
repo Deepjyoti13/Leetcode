f4b8be71ccb2973135b4ee70ddc5175e0d215815
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-22 00:30:08 +0530

    Time: 435 ms (45.96%), Space: 6.5 MB (91.38%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int dp[2001][2001];
+    int dp[2001];
     bool isPalindrome(string& s, int i, int j) {
         if(i==j) return true;
         if(i>j) return false;
@@ -11,29 +11,22 @@ class Solution {
         }
         return true;
     }
+    
     int helper(string& s, int i, int j) {
         if(i>=j) return 0;
-        if(dp[i][j]) return dp[i][j];
+        if(dp[i]) return dp[i];
         if(isPalindrome(s, i, j)) return 0;
         int res=INT_MAX;
         for(int k=i; k<j; k++) {
             if (isPalindrome(s, i, k))
  				res = min(res, 1 + helper(s, k + 1, j));
-            // res = min(res, 1+a+b);
         }
-        return dp[i][j]=res;
+        return dp[i]=res;
     }
 public:
     int minCut(string s) {
         int N = s.length();
         memset(dp, 0, sizeof(dp));
-        // int** dp = new int*[N+1];
-        // for(int i=0; i<=N; i++) {
-        //     dp[i] = new int[N+1];
-        //     for(int j=0; j<=N; j++)
-        //         dp[i][j]=0;
-        // }
-        // vector<vector<int>> dp(N+1, vector<int>(N+1, 0));
         return helper(s, 0, N-1);
     }
 };
