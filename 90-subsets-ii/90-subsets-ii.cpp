24fbdf542f22f9ca139d23e291e46dba25cd67ca
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-06-23 00:51:14 +0530

    Time: 15 ms (16.54%), Space: 7.4 MB (96.72%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,29 @@
 class Solution {
-    void helper(vector<int>& nums, int ind, int N, vector<int>& curr, vector<vector<int>>& res) {
-        res.push_back(curr);
-        for(int i=ind; i!=nums.size(); i++) {
-            if(i==ind || nums[i-1]!=nums[i]) {
-                curr.push_back(nums[i]);
-                helper(nums, i+1, N, curr, res);
-                curr.pop_back();   
-            }
+    void helper(vector<int>& nums, int ind, int prev, int N, vector<int>& ds, vector<vector<int>>& res) {
+        if(ind==N) {
+            res.push_back(ds);
+            return;
         }
+        if(ind>N) return;
+        cout<<ind<<" "<<prev<<" "<<N<<endl;
+        if(prev==-1 || nums[ind]!=nums[prev]) {
+            ds.push_back(nums[ind]);
+            helper(nums, ind+1, prev, N, ds, res);
+            ds.pop_back();
+        }
+        helper(nums, ind+1, ind, N, ds, res);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> curr;
+        if(nums.size()==1) {
+            res.push_back({});
+            res.push_back(nums);
+            return res;
+        }
+        vector<int> ds;
         sort(nums.begin(), nums.end());
-        helper(nums, 0, nums.size(), curr, res);
+        helper(nums, 0, -1, nums.size(), ds, res);
         return res;
     }
 };
