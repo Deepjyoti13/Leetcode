879ea7810b9d2d1def35835c656be4fe1d90e5ca
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-07-02 01:14:59 +0530

    Time: 80 ms (7.33%), Space: 15.5 MB (12.98%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool checkBipartite(vector<vector<int>>& grid, int src, int N, vector<int> visited)     {
+        queue<int> q;
+        q.push(src);
+        visited[src]=0;
+        vector<bool> seen(N, false);
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            seen[front]=true;
+            for(int i: grid[front]) {
+                if(visited[i]==-1) {
+                    visited[i]=visited[front]+1;
+                    q.push(i);
+                }
+                else {
+                    if(!seen[i] && visited[i]!=visited[front]+1)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int N = graph.size();
+        vector<int> visited(N, -1);
+        for(int i=0; i<N; i++) {
+            if(visited[i]==-1) {
+                if(!checkBipartite(graph, i, N, visited)) return false;
+            }
+        }
+        return true;
+    }
+};
