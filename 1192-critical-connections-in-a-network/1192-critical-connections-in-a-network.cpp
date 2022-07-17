1d6fbc02c5f4822325d7bc8c71db7be06441ab2d
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-07-17 20:07:55 +0530

    Time: 1287 ms (14.42%), Space: 196.1 MB (22.06%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    void dfs(unordered_map<int, vector<int>>& graph, vector<int>& lowest, int parent, int node, vector<vector<int>>& res, vector<bool>& visited, int nextId) {
+    void dfs(unordered_map<int, vector<int>>& graph, vector<int>& lowest, int parent, int node, vector<vector<int>>& res, int nextId) {
         // visited[node] = true;
         lowest[node] = nextId;
         nextId++;
@@ -7,7 +7,7 @@ class Solution {
         for(auto i: graph[node]) {
             if(i==parent) continue;
             if(lowest[i]==-1)
-                dfs(graph, lowest, node, i, res, visited, nextId);
+                dfs(graph, lowest, node, i, res, nextId);
             lowest[node] = min(lowest[node], lowest[i]);
             if(curr<lowest[i])
                 res.push_back({node, i});
@@ -21,9 +21,8 @@ public:
             graph[v[1]].push_back(v[0]);
         }
         vector<int> lowest(n, -1);
-        vector<bool> visited(n, false);
         vector<vector<int>> res;
-        dfs(graph, lowest, -1, 0, res, visited, 0);
+        dfs(graph, lowest, -1, 0, res, 0);
         return res;
     }
 };
