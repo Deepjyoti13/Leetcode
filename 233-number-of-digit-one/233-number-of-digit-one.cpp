a7b76b34c5c81072d33787241aea9d207884ab6f
Deepjyoti De <50995571+Deepjyoti13@users.noreply.github.com>
2022-07-26 03:13:48 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (32.51%) - LeetHub

--- 233-number-of-digit-one/233-number-of-digit-one.cpp
+++ 233-number-of-digit-one/233-number-of-digit-one.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int countDigitOne(int n) {
-        long long c=-1, temp=n, sum=0;
+        long long c=-1, temp=n, sum=0, exp, x, y, t;
         while(n) {
             c++;
-            long long t=n%10;
+            t=n%10;
             n=n/10;
             if(t==0) continue;
             else {
-                long long exp = pow(10, c);
-                long long x=t*c*exp/10;
-                long long y=0;
+                exp = pow(10, c);
+                x=t*c*exp/10;
+                y=0;
                 if(t==1) {
                     y=temp%exp+1;
                 }
